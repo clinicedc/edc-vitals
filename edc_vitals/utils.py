e3178b7fd3cb9d6fbf2e09bfe50b521c2f3f4b9c
Jonathan Willitts <JonathanWillitts@users.noreply.github.com>
2022-07-05 20:52:06 +0100

    Add TemperatureField, has_g3_fever() and has_g4_fever() (#1)
    * Add TemperatureField and has_fever_gte_g3() function
    
    * Split has_fever_gte_g3 into has_g3_fever and has_g4_fever

--- edc_vitals/utils.py
+++ edc_vitals/utils.py
@@ -29,3 +29,23 @@ def get_sys_upper():
 
 def get_dia_upper():
     return getattr(settings, "EDC_VITALS_DIA_UPPER", 110)
+
+
+def has_g3_fever(temperature=None):
+    if temperature is not None:
+        return get_g3_fever_lower() <= temperature < get_g4_fever_lower()
+    return None
+
+
+def has_g4_fever(temperature=None):
+    if temperature is not None:
+        return temperature >= get_g4_fever_lower()
+    return None
+
+
+def get_g3_fever_lower():
+    return getattr(settings, "EDC_VITALS_G3_FEVER_LOWER", 39.3)
+
+
+def get_g4_fever_lower():
+    return getattr(settings, "EDC_VITALS_G4_FEVER_LOWER", 40.0)
