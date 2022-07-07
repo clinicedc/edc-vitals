0bbef48271c344f704cfadc18ff24d0915584090
Jonathan Willitts <JonathanWillitts@users.noreply.github.com>
2022-07-07 12:06:11 +0100

    Add raise_on_systolic_lt_diastolic_bp method

--- edc_vitals/form_validators/blood_pressure_form_validator_mixin.py
+++ edc_vitals/form_validators/blood_pressure_form_validator_mixin.py
@@ -22,3 +22,18 @@ class BloodPressureFormValidatorMixin:
         if has_severe_htn(sys=avg_sys, dia=avg_dia):
             if severe_htn_response != YES:
                 raise forms.ValidationError(errmsg)
+
+    @staticmethod
+    def raise_on_systolic_lt_diastolic_bp(
+        sys_field: str = None, dia_field: str = None, **kwargs
+    ) -> None:
+        """Raise if systolic BP is < diastolic BP."""
+        sys_field = sys_field or "sys_blood_pressure"
+        dia_field = dia_field or "dia_blood_pressure"
+        sys_response = kwargs.get(sys_field)
+        dia_response = kwargs.get(dia_field)
+        if sys_response and dia_response:
+            if sys_response < dia_response:
+                raise forms.ValidationError(
+                    {dia_field: "Invalid. Diastolic must be less than systolic."}
+                )
--- edc_vitals/tests/tests/test_blood_pressure.py
+++ edc_vitals/tests/tests/test_blood_pressure.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 from edc_constants.constants import YES
 
@@ -67,6 +68,8 @@ class TestBloodPressure(TestCase):
         self.assertTrue(has_severe_htn(sys=179, dia=110))
         self.assertTrue(has_severe_htn(sys=180, dia=110))
 
+
+class TestBloodPressureFormValidatorMixin(TestCase):
     def test_bp_suggest_severe_htn_form_validator(self):
         cleaned_data = dict(
             sys_blood_pressure_one=None,
@@ -103,3 +106,88 @@ class TestBloodPressure(TestCase):
             form_validator.raise_on_avg_blood_pressure_suggests_severe_htn(**cleaned_data)
         except forms.ValidationError as e:
             self.fail(f"ValidationError unexpectedly raised. Got {e}")
+
+    def test_sys_lt_dia_bp_raises(self):
+        cleaned_data = dict(
+            sys_blood_pressure=80,
+            dia_blood_pressure=120,
+        )
+        form_validator = BloodPressureFormValidatorMixin()
+
+        with self.assertRaises(ValidationError) as cm:
+            form_validator.raise_on_systolic_lt_diastolic_bp(**cleaned_data)
+        self.assertIn("dia_blood_pressure", cm.exception.error_dict)
+        self.assertIn(
+            "Invalid. Diastolic must be less than systolic.",
+            str(cm.exception.error_dict.get("dia_blood_pressure")),
+        )
+
+        cleaned_data = dict(
+            custom_sys_blood_pressure_fld=80,
+            custom_dia_blood_pressure_fld=120,
+        )
+        form_validator = BloodPressureFormValidatorMixin()
+
+        with self.assertRaises(ValidationError) as cm:
+            form_validator.raise_on_systolic_lt_diastolic_bp(
+                sys_field="custom_sys_blood_pressure_fld",
+                dia_field="custom_dia_blood_pressure_fld",
+                **cleaned_data,
+            )
+        self.assertIn("custom_dia_blood_pressure_fld", cm.exception.error_dict)
+        self.assertIn(
+            "Invalid. Diastolic must be less than systolic.",
+            str(cm.exception.error_dict.get("custom_dia_blood_pressure_fld")),
+        )
+
+    def test_sys_gte_dia_bp_ok(self):
+        for sys, dia in [(120, 80), (180, 110), (100, 99), (90, 90)]:
+            with self.subTest(sys=sys, dia=dia):
+                cleaned_data = dict(
+                    sys_blood_pressure=sys,
+                    dia_blood_pressure=dia,
+                )
+                form_validator = BloodPressureFormValidatorMixin()
+                try:
+                    form_validator.raise_on_systolic_lt_diastolic_bp(**cleaned_data)
+                except ValidationError as e:
+                    self.fail(f"ValidationError unexpectedly raised. Got {e}")
+
+                cleaned_data = dict(
+                    custom_sys_blood_pressure_fld=sys,
+                    custom_dia_blood_pressure_fld=dia,
+                )
+                try:
+                    form_validator.raise_on_systolic_lt_diastolic_bp(
+                        sys_field="custom_sys_blood_pressure_fld",
+                        dia_field="custom_dia_blood_pressure_fld",
+                        **cleaned_data,
+                    )
+                except ValidationError as e:
+                    self.fail(f"ValidationError unexpectedly raised. Got {e}")
+
+    def test_raise_on_systolic_lt_diastolic_bp_with_none_ok(self):
+        for sys, dia in [(None, None), (None, 80), (120, None)]:
+            with self.subTest(sys=sys, dia=dia):
+                cleaned_data = dict(
+                    sys_blood_pressure=sys,
+                    dia_blood_pressure=dia,
+                )
+                form_validator = BloodPressureFormValidatorMixin()
+                try:
+                    form_validator.raise_on_systolic_lt_diastolic_bp(**cleaned_data)
+                except ValidationError as e:
+                    self.fail(f"ValidationError unexpectedly raised. Got {e}")
+
+                cleaned_data = dict(
+                    custom_sys_blood_pressure_fld=sys,
+                    custom_dia_blood_pressure_fld=dia,
+                )
+                try:
+                    form_validator.raise_on_systolic_lt_diastolic_bp(
+                        sys_field="custom_sys_blood_pressure_fld",
+                        dia_field="custom_dia_blood_pressure_fld",
+                        **cleaned_data,
+                    )
+                except ValidationError as e:
+                    self.fail(f"ValidationError unexpectedly raised. Got {e}")
