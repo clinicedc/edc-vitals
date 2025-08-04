f34206ad20fda7be363ea1c40cf3a1dd8d07287a
erikvw <ew2789@gmail.com>
2025-08-04 18:13:29 -0500

    convert to use pyproject.toml and uv

--- runtests.py
+++ runtests.py
@@ -2,4 +2,4 @@
 from edc_test_settings.func_main import func_main2
 
 if __name__ == "__main__":
-    func_main2("edc_vitals.tests.test_settings", "edc_vitals.tests")
+    func_main2("tests.test_settings", "edc_vitals.tests")
